621842292ed87778c80d762378612f3f24333a27
Roman Mazzei <mazzeiroman@gmail.com>
2020-11-23 00:40:52 -0500

    folder and files created

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the movie_planner_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database movie_planner_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table plans.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES (' Bacon Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Caracas Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Burger', FAlSE);
