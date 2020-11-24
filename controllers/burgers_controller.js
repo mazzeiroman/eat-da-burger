a2adc130c6dfae2ad98d7bcc6e6ca95d7b10b454
Roman Mazzei <mazzeiroman@gmail.com>
2020-11-24 13:01:52 -0500

    index.handlebars added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,28 +16,28 @@ router.get("/", function(req, res) {
   
   router.post("/api/burgers", function(req, res) {
     burger.insertOne(
-        ["name"], [req.body.name],
+        ["burger_name"], [req.body.name],
         function(result) {
         res.redirect("/");
     });
   });
   
-//   router.put("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
+  router.get("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
   
-//     console.log("condition", condition);
-  
-//     cat.update({
-//       sleepy: req.body.sleepy
-//     }, condition, function(result) {
-//       if (result.changedRows == 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
+    burger.updateOne({
+      devoured: true
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.redirect("/");
+        res.status(200).end();
+        
+      }
+    });
+  });
   
   
   // Export routes for server.js to use.
